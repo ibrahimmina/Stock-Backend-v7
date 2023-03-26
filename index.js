c461589618a4bf2bc9a40d23369e0e999c080529
Mina Samy Ibrahim <71660135+ibrahimmina@users.noreply.github.com>
2023-03-26 12:18:37 +0200

    U16

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
+import path from "path";
+import cors from "cors";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 const express = require("express");
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
-// Export the Express API
+
+//const __dirname = path.resolve();
+
+// Deployment configuration
+//configure env file in dev mode
+//dotenv.config();
+
+// configure env file in production
+//if (process.env.NODE_ENV === undefined) {
+//  dotenv.config({ path: "../.env" });
+//}
+
+// Connect to database
+connectDB();
+
+
+
+// Body parser
+app.use(express.json());
+
+// CORS
+//app.use(
+//  cors({
+//    origin: "*",
+//  })
+//);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// API routes
+app.use("/api/user", userRoutes);
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
 module.exports = app;
